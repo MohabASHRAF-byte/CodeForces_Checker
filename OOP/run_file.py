7a3e0a9dff32575d5e22929b2a0c0e388598dd5e
Mohab Ashraf <63811158+MohabASHRAF-byte@users.noreply.github.com>
2023-07-10 18:45:01 +0300

    Update run_file.py

--- OOP/run_file.py
+++ OOP/run_file.py
@@ -1,5 +1,7 @@
 import subprocess
 
+from timer import Timer
+
 
 class Run:
     # Put cpp file full path in the pc
@@ -8,19 +10,21 @@ class Run:
     compile_command = f"g++ {cpp_file_path} -o compiled_cpp_file"
     input_string = str()
 
-    def do_work(self, cppInput: str):
-        self.input_string = cppInput
-        # Compile the C++ file
+    def compile(self):
+        # Compile the C++ file using Clang
         compile_command = f"g++ {self.cpp_file_path} -o compiled_cpp_file"
         subprocess.run(compile_command, shell=True, check=True)
 
-        # Run the compiled C++ file
+    @staticmethod
+    def run(inp: str):
         run_command = "./compiled_cpp_file"
-        process = subprocess.Popen(run_command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        process = subprocess.Popen(run_command, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
                                    shell=True)
 
         # Pass input string to the running process
-        output, error = process.communicate(input=self.input_string.encode())
+        output, error = process.communicate(input=inp.encode())
         output = output.decode().strip()
         error = error.decode().strip()
         return len(error) != 0, error if error else output
+
